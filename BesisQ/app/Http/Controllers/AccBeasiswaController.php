d1a5dba2447b3d449da1490db5fcf280414a6c19
alfian <alfian000000@gmail.com>
2019-11-09 15:42:13 +0700

    Updated Controller Selected Beasiswa Unverified

--- BesisQ/app/Http/Controllers/AccBeasiswaController.php
+++ BesisQ/app/Http/Controllers/AccBeasiswaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 
 class AccBeasiswaController extends Controller
@@ -19,15 +20,24 @@ class AccBeasiswaController extends Controller
                 ->whereNotNull('fakultas_id')
                 ->whereNotNull('prodi_id')
                 ->where('status','=','tidak aktiv')
+                ->whereNotIn('id',function($query){
+                    $query->select('beasiswa_id')->from('beasiswa_revisi');
+                })
                 ->get();
             $beasiswaFakCuco = \App\Beasiswa::select('*')
                 ->whereNull('prodi_id')
                 ->where('status','=','tidak aktiv')
+                ->whereNotIn('id',function($query){
+                    $query->select('beasiswa_id')->from('beasiswa_revisi');
+                })
                 ->get();
             $beasiswaFree = \App\Beasiswa::select('*')
                 ->whereNull('fakultas_id')
                 ->whereNull('prodi_id')
                 ->where('status','=','tidak aktiv')
+                ->whereNotIn('id',function($query){
+                    $query->select('beasiswa_id')->from('beasiswa_revisi');
+                })
                 ->get();           
         }
         return view('admin.beasiswa.index',compact('pendaftar','isEmpty','r','beasiswaCuco','beasiswaFree','beasiswaFakCuco'));
@@ -39,5 +49,83 @@ class AccBeasiswaController extends Controller
                 'status' => 'aktiv',
             ]);
         return redirect()->back()->with('sukses','Berhasil memverifikasi Beasiswa');
+    }
+    public function decc(Request $req)
+    {
+        \App\BeasiswaRevisi::create([
+            'id' => mt_rand(100,999),
+            'beasiswa_id' => $req->idnya,
+            'jenis_kesalahan' => $req->kesalahan,
+            'catatan' => $req->catatan,
+        ]);
+        return redirect()->back()->with('sukses','Berhasil memverifikasi Beasiswa');
+    }
+    public function proses()
+    {
+        $userID = Auth::user()->id; 
+        $perusahaan = \App\Perusahaan::where('user_id','=',$userID)->first();
+
+        $r = mt_rand(1,6);
+        $s = mt_rand(1,6);
+        $t = mt_rand(1,6);
+
+        $beasiswaCuco = \App\Beasiswa::select('*')
+                ->where('perusahaan_id','=',$perusahaan->id)
+                ->whereNotNull('fakultas_id')
+                ->whereNotNull('prodi_id')
+                ->where('status','=','tidak aktiv')
+                ->whereNotIn('id',function($query){
+                    $query->select('beasiswa_id')->from('beasiswa_revisi');
+                })
+                ->get();
+        $beasiswaFakCuco = \App\Beasiswa::select('*')
+            ->where('perusahaan_id','=',$perusahaan->id)
+            ->whereNull('prodi_id')
+            ->where('status','=','tidak aktiv')
+            ->whereNotIn('id',function($query){
+                $query->select('beasiswa_id')->from('beasiswa_revisi');
+            })
+            ->get();
+        $beasiswaFree = \App\Beasiswa::select('*')
+            ->where('perusahaan_id','=',$perusahaan->id)
+            ->whereNull('fakultas_id')
+            ->whereNull('prodi_id')
+            ->where('status','=','tidak aktiv')
+            ->whereNotIn('id',function($query){
+                $query->select('beasiswa_id')->from('beasiswa_revisi');
+            })
+            ->get();       
+
+        //==BEASISWA REVISI==//
+
+        $beasiswaCucoR = \App\Beasiswa::select('*')
+            ->where('perusahaan_id','=',$perusahaan->id)
+                ->whereNotNull('fakultas_id')
+                ->whereNotNull('prodi_id')
+                ->where('status','=','tidak aktiv')
+                ->whereIn('id',function($query){
+                    $query->select('beasiswa_id')->from('beasiswa_revisi');
+                })
+                ->get();
+        $beasiswaFakCucoR = \App\Beasiswa::select('*')
+            ->where('perusahaan_id','=',$perusahaan->id)
+            ->whereNull('prodi_id')
+            ->where('status','=','tidak aktiv')
+            ->whereIn('id',function($query){
+                $query->select('beasiswa_id')->from('beasiswa_revisi');
+            })
+            ->get();
+        $beasiswaFreeR = \App\Beasiswa::select('*')
+            ->where('perusahaan_id','=',$perusahaan->id)
+            ->whereNull('fakultas_id')
+            ->whereNull('prodi_id')
+            ->where('status','=','tidak aktiv')
+            ->whereIn('id',function($query){
+                $query->select('beasiswa_id')->from('beasiswa_revisi');
+            })
+            ->get();       
+            
+
+        return view('beasiswa.proses',compact('beasiswaCuco','beasiswaFakCuco','beasiswaFree','r','s','t','beasiswaCucoR','beasiswaFakCucoR','beasiswaFreeR'));
     }
 }
