5b6b5e9111a9c1f4ecb13f05085baab5246f10e7
alfian <alfian000000@gmail.com>
2019-12-01 20:54:48 +0700

    Detail Beasiswa

--- BesisQ/resources/views/beasiswa/detail.blade.php
+++ BesisQ/resources/views/beasiswa/detail.blade.php
@@ -94,27 +94,13 @@
                   <div class="col-lg-9 col-md-9 blog_details">
                       <h2>{{$bea->nama_beasiswa}}</h2>
                       <p class="excert">
-                          MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money
-                          on boot camp when you can get the MCSE study materials yourself at a fraction.
+                          {!! $bea->beasiswaAtribut->middle_text !!}
                       </p>
-                      <p>
-                          Boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot
-                          camp when you can get the MCSE study materials yourself at a fraction of the camp price.
-                          However, who has the willpower to actually sit through a self-imposed MCSE training. who
-                          has the willpower to actually sit through a self-imposed
-                      </p>
-                      <p>
-                          Boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money on boot
-                          camp when you can get the MCSE study materials yourself at a fraction of the camp price.
-                          However, who has the willpower to actually sit through a self-imposed MCSE training. who
-                          has the willpower to actually sit through a self-imposed
-                      </p>
+                      
                   </div>
                   <div class="col-lg-12">
                       <div class="quotes">
-                          MCSE boot camps have its supporters and its detractors. Some people do not understand why you should have to spend money
-                          on boot camp when you can get the MCSE study materials yourself at a fraction of the camp
-                          price. However, who has the willpower to actually sit through a self-imposed MCSE training.
+                          {!! $bea->beasiswaAtribut->right_text !!}
                       </div>
                       <div class="row">
                           <div class="col-6">
